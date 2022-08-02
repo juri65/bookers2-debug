7c75b2f990b46da0c56906d2f0a60c1b083ddf67
EC2 Default User <106951993+juri65@users.noreply.github.com>
2022-08-02 02:27:52 +0000

    6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,7 +37,7 @@ class BooksController < ApplicationController
 
   def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
@@ -46,4 +46,11 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
+    def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(user_path) unless @user == current_user
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
     @user = User.find(params[:id])
@@ -37,4 +37,5 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end 
   end
+  
 end
