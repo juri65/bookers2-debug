cb0c5e884ce698d1e0aae09ea541afc2a0a2d1f8
EC2 Default User <106951993+juri65@users.noreply.github.com>
2022-08-06 09:33:48 +0000

    5-1

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "home/about"=>"homes#about"
- 
+  get "search" => "searches#search"
 end 
